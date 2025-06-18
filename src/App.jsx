ada5fdd07e58a569f9c31d2641519eb4c5bd6f2a
NatStri <strinattivivian@gmail.com>
2025-06-18 18:40:17 -0300

    Login de Urban Underground

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import Background from './components/Background/Background'; // Importa el componente de fondo
+import LoginForm from './components/LoginForm/LoginForm';    // Importa el componente de formulario
+import styles from '../src/components/App/App.module.css'; // <<-- IMPORTA App.module.css correctamente
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>OTRO CAMBIO RAMA PRUEBA</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    // <<-- USA LA CLASE CORRECTA DEFINIDA EN App.module.css (.app)
+    <div className={styles.app}> 
+      <Background /> {/* Renderiza el componente de fondo */}
+      <LoginForm /> {/* Renderiza el formulario de login */}
+    </div>
+  );
+};
 
-export default App
+export default App;
