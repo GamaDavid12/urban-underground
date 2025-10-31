ed76c468ae2ca08a4c8789eae58ac77e0b284d44
NatStri <strinattivivian@gmail.com>
2025-10-31 20:55:22 -0300

    se cambio el nombre Gestionar Hotspots

--- src/Mocks/adminDataMock.js
+++ src/Mocks/adminDataMock.js
@@ -1,5 +1,5 @@
 export const adminLinks = [
-    { id: 1, titulo: "Panel de Control", path: "/admin"},
+    { id: 1, titulo: "Gestionar Hotspots", path: "/admin"},
     { id: 2, titulo: "Gestionar Categorías", path: "/admin/gestionar-categorias"},  
    { id: 3, titulo: "Gestionar Productos", path: "/admin/products" },
 ];
